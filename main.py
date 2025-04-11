47995775eacef833a024954a95238c43fd1403d9
AIPLBLRNT0021\Karthik R <Karthik.r@anorasolutions.com>
2025-04-11 12:09:23 +0530

    no message

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ with open("bank_details.csv",'r') as file:
     global Account_id
     Account_id=a-1
 
-
+def write_file(account_id,message):
+    f = "Account_" + str(account_id) + ".csv"
+    with open(f,"a",newline='') as file:
+        write=csv.writer(file)
+        write.writerow([message])
 
 def add_user(account_id,name,password,amount):
     with open("bank_details.csv",'a',newline="") as file:
         field_names=['Account ID','User Name','Password','Amount']
         write=csv.DictWriter(file,fieldnames=field_names)
         write.writerow({"Account ID":account_id,"User Name":name,"Password":password,"Amount":amount})
+        f="Account_"+str(account_id)+".csv"
+        with open(f,'w',newline='') as f1:
+            write=csv.writer(f1)
+            write.writerow([f'Account created Successfully\n Welcome {name}\n Initial Deposit amount Rs {amount}'])
 
 def log_in(name,password):
     with open("bank_details.csv",'r') as file:
@@ -43,7 +51,7 @@ def operation(account_id,manipulation,amount):
                     rows[i][3]=Amount
                 elif (manipulation=="withdraw amount" or manipulation=="transfer amount") and int(rows[i][3])<amount:
                     return 0
-                elif manipulation=="withdraw amount":
+                elif manipulation=="withdraw amount" and int(rows[i][3])>=amount:
                     Amount = int(rows[i][3]) - amount
                     rows[i][3] = Amount
                 file.seek(0)
@@ -78,6 +86,12 @@ def amount_transfer(my_account,sending_account,amount):
                 write.writerows(rows)
                 break
 
+def display_file(account_id):
+    f = "Account_" + str(account_id) + ".csv"
+    with open(f,"r") as file:
+        read=csv.reader(file)
+        for i in read:
+            print(i)
 
 while(1):
     print("Select the option : \n 1) Create new Account \n 2) Log in \n 3) Exit")
@@ -102,7 +116,7 @@ while(1):
 
             if login!=0:
                 while (1):
-                    print("Select the option \n 1) Check Balance \n 2) Deposit Amount \n 3) WithDraw Amount \n 4) Transfer amount \n 5) Logout")
+                    print("Select the option \n 1) Check Balance \n 2) Deposit Amount \n 3) WithDraw Amount \n 4) Transfer amount \n 5) Display Transaction History \n 6)Logout")
                     option_2=int(input("Enter the option : "))
 
                     match option_2:
@@ -115,6 +129,7 @@ while(1):
                             deposit_amount=int(input("Enter the deposit amount : "))
                             deposit_Amount=operation(login,"deposit amount",deposit_amount)
                             print("Amount has been deposited")
+                            write_file(login,f'Deposited amount Rs {deposit_amount}')
 
                         case 3:
                             withdraw_amount=int(input("Enter the withdraw amount : "))
@@ -122,8 +137,9 @@ while(1):
                             while withdraw==0:
                                 print("Can't able to withdraw your available balance is less than the with draw amount")
                                 withdraw_amount = int(input("Renter the withdraw amount : "))
-                            withdraw = operation(login, "withdraw amount", withdraw_amount)
+                                withdraw = operation(login, "withdraw amount", withdraw_amount)
                             print("Amount with drawn successfully")
+                            write_file(login,f'Withdrawn amount Rs {withdraw_amount}')
 
                         case 4:
                             another_account=input("Enter the account id you want to send : ")
@@ -141,9 +157,13 @@ while(1):
                                 valid_amount = operation(login, "transfer amount", amount)
 
                             amount_transfer(login, another_account, amount)
-
+                            write_file(login,f'Transferred amount Rs {amount} to account {another_account}')
+                            write_file(another_account,f'Amount Rs {amount} credited from the account {login}')
 
                         case 5:
+                            display_file(login)
+
+                        case 6:
                             break
 
         case 3:
